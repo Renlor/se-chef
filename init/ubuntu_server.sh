7f4fd766eaf7e10e2c68c2599930de27aedf7470
Renlor <jvandebr@gmail.com>
2015-08-28 18:55:52 -0600

    Fixed running script from different directory breaking functionality.  Fixed another formatting issue with the prompt function.  Improved descriptions slightly on requested data fields.

--- init/ubuntu_server.sh
+++ init/ubuntu_server.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+run_dir=`pwd -P`
+
+#Get the absolute path of the script so we can load the external modules
+pushd `dirname $0` > /dev/null
+script_dir=`pwd -P`
+popd > /dev/null
+
+cd "$script_dir"
 #TODO remove globals bu writing return values to std::out and capturing them.
 source 'libs/prompt.sh'
 source 'libs/password.sh'
 #source 'libs/user.sh'
+cd "$run_dir"
 
 chef_version='12.1.2-1_amd64'
 chef_package_name="chef-server-core_$chef_version.deb"
@@ -17,11 +26,15 @@ hash wget    2>/dev/null || { echo >&2 "wget is required but is not installed.  
 hash dpkg    2>/dev/null || { echo >&2 "dpkg is required but is not installed.  Aborting."; exit 1; }
 
 # Prompt for the required values.
+echo "Select a username for your account on this chef-server."
 prompt username 'Username  : '
+echo "Create a secure password minimum length 12 characters."
 get_password password 12
+echo "Enter your information."
 prompt firstname 'First Name: '
 prompt lastname  'Last Name : '
 prompt email     'Email     : '
+echo "Enter the information about your organization."
 prompt org_shortname 'Organization Short Name: '
 prompt org_longname  'Full Organization Name : '
 
@@ -30,19 +43,21 @@ sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::=--force-confnew upgrade
 sudo apt-get -y -o Dpkg::Options::=--force-confnew dist-upgrade
 
+sudo apt-get -y install postgresql git
+
 #TODO get latest chef version dynamically
 #wget -np 'https://downloads.chef.io/chef-server/ubuntu/#/'
 #grep oc-init-product-data 'index.html' > 'product-data'
 #cat 'product-data' | cut -d ''
 
 # Download Chef-Server
-wget $chef_package_url
+wget "$chef_package_url"
 #TODO run checksum
 
 # Install Chef-Server
-sudo dpkg -i $chef_package_name
+sudo dpkg -i "$chef_package_name"
 #TODO check if install succeeded before continuing.
-rm $chef_package_name
+rm "$chef_package_name"
 sudo chef-server-ctl reconfigure
 
 #TODO finish setup with a configured user
@@ -51,5 +66,5 @@ sudo chef-server-ctl reconfigure
 #exit $?
 
 mkdir .chef
-sudo chef-server-ctl user-create $username $firstname $lastname $email $password --filename ~/.chef/$username.pem
-sudo chef-server-ctl org-create $org_shortname $org_fullname --association_user $username --filename ~/.chef/$shortname.pem
+sudo chef-server-ctl user-create "$username" "$firstname" "$lastname" "$email" "$password" --filename "~/.chef/$username.pem"
+sudo chef-server-ctl org-create "$org_shortname" "$org_fullname" --association_user "$username" --filename "~/.chef/$shortname.pem"

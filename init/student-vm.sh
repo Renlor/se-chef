895c9961201e23f4371f7990ac6ae46ee0282f1a
Renlor <Renlor@liddev.com>
2015-08-31 23:07:23 -0600

    Remainder of previous commit.

--- init/student-vm.sh
+++ init/student-vm.sh
@@ -13,6 +13,42 @@ source 'lib/prompt.sh' #user prompt utilities.
 source 'lib/user.sh'   #*nix user manipulation library.
 cd ${run_dir}
 
+while [[ $# > 1 ]]; do
+  key="$1"
+
+  case $key in
+      -e|--environment|--env)
+      environment="$2"
+      shift # past argument
+      ;;
+      *)
+              # unknown option
+      ;;
+  esac
+  shift # past argument or value
+done
+
+if [ "$environment" == "prod" ]; then
+  :
+elif [  "$environment" == "test"  ]; then
+  :
+elif [  "$environment" == "dev"  ]; then
+  :
+else
+  echo "ERROR:   Environment is not set to a valid value. Please set it to one of the following:"
+  echo -e "\tprod : For production.  This is by far the safest setting."
+  echo -e "\t\tIf you are unsure which environment to choose use this one."
+  echo
+  echo -e "\ttest : For testing new features.  This will contain the latest \"working\" features."
+  echo -e "\t\tOnly choose this setting if you know how to fix an issue if it arises."
+  echo -e "\t\tThis setting is mainly used as a staging area for changes destined for production."
+  echo
+  echo -e "\t dev : For developing new features. For use in development of this repo and related projects."
+  echo -e "\t\tBy far the most dangerous setting, expect anything and everything to be broken at any time."
+  echo
+  exit 1
+fi
+
 #TODO check if this is a supported linux distrobution.
 hash sudo    2>/dev/null || { echo >&2 "sudo is required but is not installed.  Aborting."; exit_script='1'; }
 hash apt-get 2>/dev/null || { echo >&2 "apt-get is required but is not installed.  Aborting."; exit_script='1'; }
@@ -35,10 +71,10 @@ sudo apt-get -y install chef git build-essential wget chef-dk
 sudo rm -rf /var/chef-solo
 
 sudo mkdir '/var/chef-solo'
-sudo git clone -b 'production' 'https://github.com/uccs-se/chef' '/var/chef-solo'
+sudo git clone -b ${environment} 'https://github.com/uccs-se/chef' '/var/chef-solo'
 sudo cd /var/chef-solo
 sudo bundle install
 sudo berks install
 # Set up chef system control.
-sudo chef-solo -c /var/chef-solo/cookbooks/student_vm/files/ubuntu/solo.rb -j /var/chef-solo/cookbooks/student_vm/files/ubuntu/production.json -E production
+sudo chef-solo -c /var/chef-solo/cookbooks/student_vm/files/ubuntu/solo.rb -j /var/chef-solo/cookbooks/student_vm/files/ubuntu/production.json -E ${environment}
 # the rest is up to chef.

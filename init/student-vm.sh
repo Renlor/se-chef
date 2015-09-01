db4379493a4ab9dc7dd485c419ebfd979d6d1cd9
Renlor <jvandebr@gmail.com>
2015-08-31 23:28:52 -0600

    Install chefdk manually.

--- init/student-vm.sh
+++ init/student-vm.sh
@@ -66,9 +66,13 @@ sudo apt-get -y -o DPkg::Options::="--force-confnew" upgrade
 sudo apt-get -y -o DPkg::Options::="--force-confnew" dist-upgrade
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -y install chef git build-essential wget chef-dk
+sudo apt-get -y install chef git build-essential wget
 
 sudo rm -rf /var/chef-solo
+
+wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/13.10/x86_64/chefdk_0.7.0-1_amd64.deb -o chefdk_0.7.0-1_amd64.deb
+sudo dpkg -i chefdk_0.7.0-1_amd64.deb
+rm chefdk_0.7.0-1_amd64.deb
 
 #TODO: install current system ruby
 
